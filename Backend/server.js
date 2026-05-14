d905379411134e11222366c1dfd10c315f790606
Pooja Jha <poojajha28102001@gmail.com>
2026-05-14 23:53:15 +0530

    fix

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,10 @@ const routes = require('./routes');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
